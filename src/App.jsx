a220ec6841def5d5f935e921fe75e49e8b21f6d1
Shane Franklin <shanefranklin148@gmail.com>
2025-11-26 13:06:13 -0500

    Simple dice roller setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,88 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [selectedValue, setSelectedvalue] = useState("1d6");
+  const [diceRoll, setDiceRoll] = useState(null);
+
+  const handleChange = (event) => {
+    setSelectedvalue(event.target.value);
+  };
+
+  const handleClick = () => {
+    setDiceRoll(Math.floor(Math.random() * parseInt(selectedValue)) + 1);
+  };
+
   return (
     <>
-      <form>
+      <div>
         <label>
-          <input type="radio" name="diceType" value="red" id="six" /> Red
+          <input
+            type="radio"
+            name="1d2"
+            value="2"
+            checked={selectedValue === "2"}
+            onChange={handleChange}
+          />
+          1D2
         </label>
         <label>
-          <input type="radio" name="diceType" value="blue" id="eight" /> Blue
+          <input
+            type="radio"
+            name="1d4"
+            value="4"
+            checked={selectedValue === "4"}
+            onChange={handleChange}
+          />
+          1D4
         </label>
         <label>
-          <input type="radio" name="diceType" value="green" id="twenty" /> Green
+          <input
+            type="radio"
+            name="1d6"
+            value="6"
+            checked={selectedValue === "6"}
+            onChange={handleChange}
+          />
+          1d6
         </label>
-      </form>
+        <label>
+          <input
+            type="radio"
+            name="1d8"
+            value="8"
+            checked={selectedValue === "8"}
+            onChange={handleChange}
+          />
+          1D8
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="1d10"
+            value="10"
+            checked={selectedValue === "10"}
+            onChange={handleChange}
+          />
+          1D10
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="1d20"
+            value="20"
+            checked={selectedValue === "20"}
+            onChange={handleChange}
+          />
+          1D20
+        </label>
+      </div>
+      <div>
+        <button onClick={handleClick}>Roll Dice!</button>
+      </div>
+      <div>
+        <span>{diceRoll}</span>
+      </div>
     </>
   );
 }
