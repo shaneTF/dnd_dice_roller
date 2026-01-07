1ec94ba044dcf44c3c17507b18730f0807dd43fc
Shane Franklin <shanefranklin148@gmail.com>
2026-01-06 19:45:36 -0500

    Added simple parse for pdf doc.

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,16 @@ import {
   DiceTwenty,
 } from "./components/dice";
 import ImportButton from "./components/importButton";
+import * as pdfjsLib from "pdfjs-dist";
+import pdfjsWorder from "pdfjs-dist/build/pdf.worker.min.mjs?url";
 import "./App.css";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorder;
 
 function App() {
   const [selectedValue, setSelectedvalue] = useState("6");
   const [diceRoll, setDiceRoll] = useState(null);
+  const [parsedText, setParsedText] = useState("");
 
   const handleChange = (event) => {
     setSelectedvalue(event.target.value);
@@ -23,8 +28,23 @@ function App() {
     setDiceRoll(Math.floor(Math.random() * parseInt(selectedValue)) + 1);
   };
 
-  const handleFile = (file) => {
+  const handleFile = async (file) => {
     console.log("Selected file:", file);
+    const arrayBuffer = await file.arrayBuffer();
+    const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
+    console.log(pdf);
+
+    let fullText = "";
+
+    for (let i = 1; i <= pdf.numPages; i++) {
+      const page = await pdf.getPage(i);
+      const content = await page.getTextContent();
+      const strings = content.items.map((item) => item.str);
+      fullText += strings.join(" ") + "\n\n";
+    }
+
+    console.log("Extracted Text:", fullText);
+    setParsedText(fullText);
   };
 
   return (
