452d29cfb9c7e817298cc0b76f9939a82d2e6a3b
Shane Franklin <shanefranklin148@gmail.com>
2026-02-03 18:40:14 -0500

    Changed useEffect to useMemo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState, useMemo } from "react";
 import {
   DiceTwo,
   DiceFour,
@@ -13,6 +13,8 @@ import "./App.css";
 function App() {
   const [selectedValue, setSelectedvalue] = useState("6");
   const [diceRoll, setDiceRoll] = useState(null);
+  const [statMod, setStatMod] = useState(null);
+  const [diceRolled, setDiceRolled] = useState(false);
   const [stats, setStats] = useState({
     str: "",
     dex: "",
@@ -21,30 +23,20 @@ function App() {
     wis: "",
     cha: "",
   });
-  const [statModifier, setStatModifier] = useState({
-    str: 0,
-    dex: 0,
-    con: 0,
-    int: 0,
-    wis: 0,
-    cha: 0,
-  });
 
-  useEffect(() => {
-    const newStatModifier = {};
-    Object.keys(stats).forEach((statKey) => {
-      const statValue = stats[statKey] || 0;
-      newStatModifier[statKey] =
-        parseInt(statValue) == 0 ? 0 : Math.floor((statValue - 10) / 2);
-    });
-    Object.keys(newStatModifier).forEach((key) => {
-      if (statModifier[key] !== newStatModifier[key]) {
-        setStatModifier(newStatModifier);
-      }
-    });
-  }, [stats, statModifier]);
+  const statModifier = useMemo(() => {
+    return Object.fromEntries(
+      Object.entries(stats).map(([key, value]) => {
+        const score = parseInt(value) || 0;
+        const mod = score === 0 ? 0 : Math.floor((score - 10) / 2);
+        setStatMod(mod);
+        return [key, mod];
+      }),
+    );
+  }, [stats]);
 
   const diceMath = () => {
+    setDiceRolled(true);
     return Math.floor(Math.random() * parseInt(selectedValue)) + 1;
   };
 
@@ -60,6 +52,7 @@ function App() {
     const baseRoll = diceMath();
     console.log("Base Roll: ", baseRoll);
     console.log("Stat Modifier: ", statModifier[statKey]);
+    setStatMod(statModifier[statKey]);
     setDiceRoll(baseRoll + parseInt(statModifier[statKey]));
   };
 
@@ -89,6 +82,13 @@ function App() {
       </div>
       <div>
         <button onClick={handleClick}>Roll Dice!</button>
+      </div>
+      <div>
+        {diceRolled && (
+          <span>
+            {statMod} + {diceRoll}
+          </span>
+        )}
       </div>
       <div>
         <span className="roll-result">{diceRoll}</span>
